238fa731cc0e1931071f71ab1a76afdbef4f4fc7
Bob Crawford <recrawford@burnsmcd.com>
2018-11-09 22:48:34 +0000

    UI work

--- DataGeneratorPi/MainWindow.xaml.cs
+++ DataGeneratorPi/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OSIsoft.AF;
+using OSIsoft.AF.Search;
 
 namespace DataGeneratorPi
 {
@@ -20,9 +22,60 @@ namespace DataGeneratorPi
     /// </summary>
     public partial class MainWindow : Window
     {
+        PISystems systems = new PISystems();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadServers();
+        }
+
+        private void LoadServers()
+        {
+            cmbServers.ItemsSource = systems;
+            if (systems.Count == 1)
+            {
+                cmbServers.SelectedIndex = 0;
+            }
+        }
+
+        private void cmbServers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                var system = (PISystem)e.AddedItems[0];
+                cmbDatabases.ItemsSource = system.Databases;
+            }
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbDatabases.SelectedItem is AFDatabase db)
+            {
+                var query = @"Element:{ Name:'*' } " + $"{txtAttributeSearch.Text}*";
+                using (var search = new AFAttributeSearch(db, "AttrSearch", query))
+                {
+                    search.CacheTimeout = TimeSpan.FromMinutes(10);
+
+                    var attributes = search.FindAttributes();
+                    dgSearchResults.Items.Clear();
+                    var res = from a in attributes
+                              where a.DataReference != null
+                              select new SearchResult
+                              {
+                                  ElementName = a.Element.Name,
+                                  AttributeName = a.Name,
+                                  DataType = a.Type.Name,
+                                  _self = a
+                              };
+
+                    dgSearchResults.ItemsSource = res.ToList();
+                }
+            }
         }
     }
 }
---
+++ DataGeneratorPi/SearchResult.cs
@@ -0,0 +1,17 @@
+﻿using OSIsoft.AF.Asset;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGeneratorPi
+{
+    public class SearchResult
+    {
+        public string ElementName { get; set; }
+        public string AttributeName { get; set; }
+        public string DataType { get; set; }
+        public AFAttribute _self { get; set; }
+    }
+}
